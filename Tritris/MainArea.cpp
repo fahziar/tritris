c07888362954fba761ad667003e6e77ad3e40453
fahziar <fahziar_rw@yahoo.co.uk>
2014-08-30 22:11:11 +0700

    Main Area sudah diimplementasi

---
+++ Tritris/MainArea.cpp
@@ -0,0 +1,115 @@
+#include "MainArea.h"
+
+mainArea::mainArea(int nbCol, int nbRow) :nbCol(nbCol), nbRow(nbRow), cells(nbCol * nbRow)
+{
+	int col, row;
+	for (col = 0; col < nbCol; col++)
+	{
+		for (row = 0; row < nbRow; row++)
+		{
+			cells.at(getAt(col, row)).setX(col);
+			cells.at(getAt(col, row)).setY(row);
+		}
+	}
+}
+
+void mainArea::draw()
+{
+	int i, j;
+
+	for (i = 0; i < nbCol; i++)
+	{
+		for (j = 0; j < nbRow; j++)
+		{
+			this->cells.at(i * j).draw();
+		}
+	}
+}
+
+int mainArea::getAt(int col, int row)
+{
+	return (row * nbCol) + col;
+}
+
+bool mainArea::moveAllTriangleDownward(int colSource, int rowSource)
+{
+	//cek apakah cell dibawahanya fit
+	if (cells.at(getAt(colSource, rowSource)).getNbTriangle() == 2)
+	{
+		if (!cells.at(getAt(colSource, rowSource + 1)).isEmpty())
+		{
+			return false;
+		}
+	}
+	else if (cells.at(getAt(colSource, rowSource)).getNbTriangle() == 1)
+	{
+		if (!cells.at(getAt(colSource, rowSource + 1)).isFitFromAbove(cells.at(getAt(colSource, rowSource)).getTriangle(0)))
+		{
+			return false;
+		}
+	}
+
+	//Pindahin semua ke bawah
+	int i, nbTriangle;
+	nbTriangle = cells.at(getAt(colSource, rowSource)).getNbTriangle();
+	for (i = 0; i < nbTriangle; i++)
+	{
+		triangle temp = cells.at(getAt(colSource, rowSource)).getTriangle(0);
+		cells.at(getAt(colSource, rowSource)).removeTriangle(0);
+		cells.at(getAt(colSource, rowSource + 1)).addTriangle(temp);
+	}
+	
+}
+
+void mainArea::setTriangle(int x, int y, triangle t)
+{
+	cells.at(getAt(x, y)).addTriangle(t);
+}
+
+triangle mainArea::removeTriangle(int x, int y)
+{
+	triangle out = cells.at(getAt(x, y)).getTriangle(0);
+	cells.at(getAt(x, y)).removeTriangle(0);
+	return out;
+}
+
+bool mainArea::isLineFullOfTriangle(int row)
+{
+	int i = 0;
+	bool full = true;
+
+	while ((full) && (i < this->nbCol))
+	{
+		if (cells.at(getAt(i, row)).getNbTriangle() < 2)
+		{
+			full = false;
+		}
+		i++;
+	}
+
+	return full;
+}
+
+void mainArea::removeAllTrianglesInRow(int row)
+{
+	//Remove all triangles
+	int i;
+	for (i = getAt(0, row); i < getAt(0, row + 1); i++)
+	{
+		int j;
+		int nbTriangle = cells.at(i).getNbTriangle();
+		for (j = 0; j < nbTriangle ; j++)
+		{
+			cells.at(i).removeTriangle(0);
+		}
+	}
+
+	//Geser semua segitiga yang ada di atas row ke bawah
+	for (i = row - 1; i >= 0; i--)
+	{
+		int j;
+		for (j = 0; j < nbCol; j++)
+			moveAllTriangleDownward(j, i);
+	}
+
+}
---
+++ Tritris/MainArea.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_AREA_H
+#define MAIN_AREA_H
+#include <vector>
+#include "MainAreaCell.h"
+#include "Triangle.h"
+
+class mainArea
+{
+private:
+	int nbRow, nbCol;
+	std::vector <mainAreaCell> cells;
+	int getAt(int col, int row);
+public:
+	mainArea(int nbCol, int nbRow);
+
+	void draw();
+	bool moveAllTriangleDownward(int colSource, int rowSource);
+	triangle getCell(int x, int y);
+	void setTriangle(int x, int y, triangle t);
+	triangle removeTriangle(int x, int y);
+
+	bool isLineFullOfTriangle(int row);
+	void removeAllTrianglesInRow(int row);
+};
+
+#endif
